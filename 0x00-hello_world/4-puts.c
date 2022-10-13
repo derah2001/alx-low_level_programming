40f7ccff67844912ff6b5731d2ba10068e6c3360
Chidera Nwosu <derahnwosu@gmail.com>
2022-10-14 00:58:38 +0100

    updating 4-puts.c using betty style and doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * puts - Print Str
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 
-    char Str[] = "Programming is like building a multilingual puzzle";
+char Str[] = "\"Programming is like building a multilingual puzzle";
 
-    puts(Str);
-    return 0;
+puts(Str);
+return (0);
 }    
