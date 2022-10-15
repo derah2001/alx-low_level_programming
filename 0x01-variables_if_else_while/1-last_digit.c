b11c4236556a984c949593854ab27cdb5a052bc3
Chidera Nwosu <derahnwosu@gmail.com>
2022-10-15 01:12:27 +0100

    change the last if function argument

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ else
 if (n == 0)
 printf("Last digit of %d is %d and is zero\n", n, (n%10));
 else
-if (6 > n > 0)
+if (n < 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n%10));	
 return (0);
 }
