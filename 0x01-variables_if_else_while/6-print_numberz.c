8350ca5b53a1cebb74b91b82247e856a681341a5
Chidera Nwosu <derahnwosu@gmail.com>
2022-10-14 20:22:15 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Rteurn: Always 0 (Success)
+ */
+int main(void)
+{
+int nums = 48;
+while (nums <= 57)
+putchar(nums);
+nums++;
+}
+putchar('\n');
+return (0);
+}
