7c413d314d63d986704e679ea2addd8b6595bd49
Chidera Nwosu <derahnwosu@gmail.com>
2022-10-14 19:55:36 -0700

    printing 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 48;
+while (48 <= 57)
+{
+printf("%c", num);
+num++;
+}
+printf("\n");
+return (0);
+}
