d2092158ad877b7f882adf33a8f42e922de24b9f
Chidera Nwosu <derahnwosu@gmail.com>
2022-10-14 18:39:28 -0700

    betty style corrected

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-/*
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+/**
+ * main - Entry point
+ * while - function for printing ascii code
+ * putchar - printing the ascii code to alphabets
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char letter = 97;
