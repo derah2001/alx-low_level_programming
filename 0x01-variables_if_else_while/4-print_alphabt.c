7c68a1486e1257b811dac395dd9cf48a99286e48
Chidera Nwosu <derahnwosu@gmail.com>
2022-10-15 03:46:07 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,7 @@ int main(void)
 char letter = 97;
 while (letter <= 122)
 {
-if (letter == 101)
-continue;
-if (letter == 113)
-continue;
+if (letter != 101 && letter != 113)
 putchar(letter);
 letter++;
 }
